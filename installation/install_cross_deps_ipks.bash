9678c69a906ec33dc9c3f89ea91fc09f67ee55b4
Matthew Reynolds <mtreynolds@uwaterloo.ca>
2018-11-24 11:10:28 -0500

    Add cross-compilation scripts and instructions

---
+++ installation/install_cross_deps_ipks.bash
@@ -0,0 +1,106 @@
+#!/bin/bash
+#
+# Script used to install dependencies needed to cross-compile ROS.
+# Whenever possible, we prefer to install IPKs that NI already distributes.
+# However, when these aren't available, we instead build the dependencies from source.
+#
+# This script handles the dependencies that can be installed from NI's IPKs
+
+## IMPORTANT NOTE: The 2018 frc_toolchain uses GCC 5.5.
+# Due to ABI compatibility, this means we can only use libraries compiled with
+# 5.5 or earlier. This means we CANNOT use the 2018 ipks from NI, since they're built
+# on GCC 6.3. However, the 2017 ipks are safe, as they are built on GCC 5.3.
+# In general, we have to just ensure we're using a valid version of the ni-linux-rt
+# libraries
+
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit
+fi
+
+function install_package ()
+{
+	mkdir ~/.frc_control_temp
+	cd ~/.frc_control_temp
+	wget -q --show-progress "$1"
+	ar x "${1##*/}" # Note: This regex simply extracts the filename from the url
+	tar xzf data.tar.gz -C /usr/arm-frc-linux-gnueabi
+	cd
+	rm -rf .frc_control_temp
+}
+
+prefix="http://download.ni.com/ni-linux-rt/feeds/2017/arm/cortexa9-vfpv3/"
+
+## Install Boost
+install_package "${prefix}boost-dev_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+# install_package "${prefix}boost-log_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+# install_package "${prefix}boost-serialization_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+# install_package "${prefix}boost-staticdev_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+# install_package "${prefix}boost-test_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+# install_package "${prefix}boost_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+install_package "${prefix}libboost-atomic1.60.0_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+install_package "${prefix}libboost-chrono1.60.0_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+install_package "${prefix}libboost-date-time1.60.0_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+install_package "${prefix}libboost-filesystem1.60.0_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+install_package "${prefix}libboost-graph1.60.0_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+install_package "${prefix}libboost-iostreams1.60.0_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+install_package "${prefix}libboost-program-options1.60.0_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+install_package "${prefix}libboost-random1.60.0_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+install_package "${prefix}libboost-regex1.60.0_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+install_package "${prefix}libboost-signals1.60.0_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+install_package "${prefix}libboost-system1.60.0_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+install_package "${prefix}libboost-thread1.60.0_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+install_package "${prefix}libboost-timer1.60.0_1.60.0-r0.3_cortexa9-vfpv3.ipk"
+
+## Install Python
+install_package "${prefix}python-core_2.7.11-r1.49_cortexa9-vfpv3.ipk"
+install_package "${prefix}libpython2_2.7.11-r1.49_cortexa9-vfpv3.ipk" # Required for python_orocos_kdl
+install_package "${prefix}python-dev_2.7.11-r1.49_cortexa9-vfpv3.ipk" # Required for python_orocos_kdl
+
+## Install Eigen (Required for various packages (eigen, orocos, kdl, etc))
+install_package "${prefix}libeigen_3.2.6-r0.5_cortexa9-vfpv3.ipk"
+
+# Install FindEigen3.cmake
+# Replaces deprecated FindEigen.cmake provided by the ROS cmake_modules package.
+# Note that we then need to manually specify the CMAKE_MODULE_PATH to include /usr/arm-frc-linux-gnueabi/usr/share/cmake/Modules 
+# See https://github.com/ros-perception/perception_pcl/issues/106
+# For some reason, frcmake (cmake 3.5.1) doesn't have /usr/arm-frc-linux-gnueabi/usr/share/cmake/Modules in its modules path by default
+install_package "${prefix}libeigen-dev_3.2.6-r0.5_cortexa9-vfpv3.ipk"
+
+# Install cURL (Required for resource_retriever)
+install_package "${prefix}libcurl4_7.51.0-r0.3_cortexa9-vfpv3.ipk"
+install_package "${prefix}curl-dev_7.51.0-r0.3_cortexa9-vfpv3.ipk"
+
+# Install lz4 (Required for roslz4)
+install_package "${prefix}lz4_131+git0+d86dc91677-r0.4_cortexa9-vfpv3.ipk"
+install_package "${prefix}lz4-dev_131+git0+d86dc91677-r0.4_cortexa9-vfpv3.ipk"
+
+# Install BZip2 (Required for rosbag_storage)
+install_package "${prefix}libbz2-1_1.0.6-r5.326_cortexa9-vfpv3.ipk"
+install_package "${prefix}bzip2-dev_1.0.6-r5.326_cortexa9-vfpv3.ipk"
+
+# Install UUID (Required for bondcpp)
+# Unfortunately, only the library and the binary are included, not dev headers. Perhaps we should look into libossp-uuid later.
+# For now, we just compile from source instead
+#install_package "${prefix}libuuid1_2.27.1-r0.3.61_cortexa9-vfpv3.ipk"
+
+# Install ZLIB (Required for collada-dom)
+# TODO: Double check this is required
+install_package "${prefix}libz-dev_1.2.8-r0.329_cortexa9-vfpv3.ipk"
+install_package "${prefix}libz1_1.2.8-r0.329_cortexa9-vfpv3.ipk"
+
+# Install libxml2 (Required for collada-dom)
+install_package "${prefix}libxml2-dev_2.9.4-r0.49_cortexa9-vfpv3.ipk"
+install_package "${prefix}libxml2_2.9.4-r0.49_cortexa9-vfpv3.ipk"
+
+# Install libgnutls (Required by collada_urdf)
+install_package "${prefix}libgnutls30_3.4.9-r0.7_cortexa9-vfpv3.ipk"
+
+# Install libidn (Required by collada_urdf)
+install_package "${prefix}libidn11_1.32-r0.7_cortexa9-vfpv3.ipk"
+
+# Install libnettle (Required by collada_urdf)
+install_package "${prefix}nettle_3.2-r0.7_cortexa9-vfpv3.ipk"
+
+# Install libgmp (Required by collada_urdf)
+install_package "${prefix}libgmp10_6.1.0-r0.44_cortexa9-vfpv3.ipk"
---
+++ installation/install_cross_deps_src.bash
@@ -0,0 +1,175 @@
+#!/bin/bash
+#
+# Script used to install dependencies needed to cross-compile ROS.
+# Whenever possible, we prefer to install IPKs that NI already distributes.
+# However, when these aren't available, we instead build the dependencies from source.
+#
+# This script handles the dependencies that must be built from source.
+#
+
+# TODO: Consider building libraries as static to make it easier to install on the RIO
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+make_args="-S"
+
+##### Launch Options #####
+
+while [ $# -gt 0 ];
+do
+    case "$1" in
+        -h|--help)
+            echo "Options:"
+            echo "-t FILE, --toolchain=FILE   = Specify the location of the toolchain file. If unspecified, default location used (Relative to script)"
+            echo "-j [N], --jobs[=N]          = Specify the number of jobs to use when compiling with make. If unspecified, infinite jobs"
+            echo "-q, --quiet                 = Don't echo make recepies"
+            exit 0
+            ;;
+
+        # Toolchain file
+        -t)
+            shift
+            toolchain="$1"
+            shift
+            ;;
+        --toolchain*)
+            toolchain=`echo $1 | sed -e 's/^[^=]*=//g'`
+            shift
+            ;;
+
+        # Number of jobs
+        -j)
+            shift
+            num_jobs="$1"
+            shift
+            ;;
+        --jobs*)
+            num_jobs=`echo $1 | sed -e 's/^[^=]*=//g'`
+            shift
+            ;;
+        
+        # Verbosity
+        -mq)
+            ;&
+        --make_quiet)
+            make_args+=" --quiet"
+            shift
+            ;;
+
+        # Unknown option
+        *)
+            echo "Unknown option ${1}"
+            exit 1
+            ;;
+    esac
+done
+
+##### Main script #####
+
+# If the user specified a number of jobs, ensure it is a valid integer
+if ! [ -z "$num_jobs" ]; then
+    if ! [[ "$num_jobs" =~ ^[0-9]+$ ]] || [ $num_jobs -lt 1 ]; then
+    echo "Error: $num_jobs is not a positive integer" >&2
+    exit 1
+    fi
+fi
+
+# Ensure the user-specified toolchain file. If they didn't provide one, use the default (relative to this script)
+if [ -z "$toolchain" ]; then
+    toolchain="${script_dir}/../rostoolchain.cmake"
+fi
+if [ ! -f "$toolchain" ]; then
+    echo "Toolchain file "$toolchain" not found!"
+    exit 1
+fi
+
+# Ensure the user is running as root
+if (( $EUID != 0 )); then
+    echo "Please run as root"
+    exit 1
+fi
+
+# Used whenever a standard CMake compile is possible
+# $1 is the name of the library, $2 is the url
+function install_cmake ()
+{
+    archive="${1}_${2##*/}" # Prepend the archive name with the name of the library
+	wget -q --show-progress $2 -O $archive
+    mkdir -p ${1}/build
+	tar xzf $archive --strip 1 -C $1
+    cd ${1}/build
+    cmake -DCMAKE_TOOLCHAIN_FILE="$toolchain" -DCMAKE_INSTALL_PREFIX=/usr/arm-frc-linux-gnueabi -DCMAKE_BUILD_TYPE=Release ..
+    make install $make_args -j $num_jobs
+    cd ../..
+}
+
+# Create temp directory to work in
+rm -rf ~/.frc_control_temp
+mkdir ~/.frc_control_temp
+cd ~/.frc_control_temp
+
+# class_loader v0.3.9 (Mar 16 2018) requires console_bridge. Corresponding console_bridge version for this date: v0.4.0
+# tf2 v0.5.19 (Nov 17 2018) requires console_bridge. However, **tf2 requires v0.3.2**
+install_cmake console_bridge https://github.com/ros/console_bridge/archive/0.3.2.tar.gz
+
+# pluginlib v1.11.3 (Mar 16 2018) requires TinyXML2. Corresponding TinyXML2 version for this date: v6.0.0
+install_cmake TinyXML2 https://github.com/leethomason/tinyxml2/archive/6.0.0.tar.gz
+
+# TODO: urdfdom and urdfdom_headers can be installed either manually like this, or by merging them into the ros workspace and cloning their package.xml files. Not sure which is better
+# urdf_parser_plugin v1.12.12 (Nov 8 2017) requires urdfdom. Corresponding version v1.0.0
+install_cmake urdfdom https://github.com/ros/urdfdom/archive/1.0.0.tar.gz
+
+# urdf_parser_plugin v1.12.12 (Nov 8 2017) requires urdfdom_headers. Corresponding version v1.0.0
+install_cmake urdfdom_headers https://github.com/ros/urdfdom_headers/archive/1.0.0.tar.gz
+
+# geometric_shapes v0.5.4 (May 14 2018) requires qhull. Corresponding version v2015.2
+install_cmake qhull http://www.qhull.org/download/qhull-2015-src-7.2.0.tgz
+
+# geometric_shapes v0.5.4 (May 14 2018) requires assimp. Corresponding version v4.1.0
+install_cmake assimp https://github.com/assimp/assimp/archive/v4.1.0.tar.gz
+
+# tf v1.11.9 (Jul 14 2017) requires gtest. Corresponding version v1.8.0
+install_cmake googletest https://github.com/google/googletest/archive/release-1.8.0.tar.gz
+
+# collada_parser v1.12.12 (May 8 2018) requires collada-dom. Corresponding version v2.5.0
+install_cmake colllada-dom https://github.com/rdiankov/collada-dom/archive/v2.5.0.tar.gz
+
+# class_loader v0.3.9 (Mar 16 2018) requires Poco. Corresponding Poco version for this date: v1.9.0
+wget -q --show-progress https://github.com/pocoproject/poco/archive/poco-1.9.0-release.tar.gz
+tar xzf poco-1.9.0-release.tar.gz
+cd poco-poco-1.9.0-release
+CROSS_COMPILE=arm-frc-linux-gnueabi- ./configure --no-tests --no-samples --minimal --prefix=/usr/arm-frc-linux-gnueabi/usr/local
+CROSS_COMPILE=arm-frc-linux-gnueabi- make install $make_args -j $num_jobs --quiet #We get TONS of warnings here if we don't compile quietly
+
+# python_orocos_kdl v1.3.1 (Apr 5 2016) requires SIP. Corresponding SIP version for this date: v4.17
+wget -q --show-progress https://sourceforge.net/projects/pyqt/files/sip/sip-4.17/sip-4.17.tar.gz
+tar xzf sip-4.17.tar.gz
+cd sip-4.17
+python configure.py CC=arm-frc-linux-gnueabi-gcc CXX=arm-frc-linux-gnueabi-g++ LINK=arm-frc-linux-gnueabi-g++ LINK_SHLIB=arm-frc-linux-gnueabi-g++ --sysroot=/usr/arm-frc-linux-gnueabi --incdir=/usr/arm-frc-linux-gnueabi/usr/include/python2.7 STRIP=arm-frc-linux-gnueabi-strip
+sed -i '/^CPPFLAGS/ s_include_usr/include_' siplib/Makefile
+make install $make_args -j $num_jobs
+
+# rospack v2.4.4 (Nov 16 2017) requires TinyXML. Corresponding TinyXML version for this date: v2.6.2
+wget -q --show-progress https://sourceforge.net/projects/tinyxml/files/tinyxml/2.6.2/tinyxml_2_6_2.tar.gz
+tar xzf tinyxml_2_6_2.tar.gz
+cd tinyxml
+# TinyXML's makefile is for gnu make. We need to create a CMakeLists. An existing template exists, so we will pull that
+wget -q https://gist.githubusercontent.com/TNick/7960323/raw/3046ecda1d4d54d777c407f43ac357846a192e05/TinyXML-CmakeLists.txt -O CMakeLists.txt
+# Force installation of the header file
+sed -i "14i  set_target_properties(tinyxml PROPERTIES PUBLIC_HEADER tinyxml.h)" CMakeLists.txt
+# Since we're installing the header, we also need to ensure the header and the library match. We will override the ENABLE_STL behaviour
+# by forcing STL to be enabled in the header. This ensures that both the library and anyone using the header will have STL enabled
+sed -i "29i#ifndef TIXML_USE_STL\n\t#define TIXML_USE_STL\n#endif\n" tinyxml.h
+cmake -DCMAKE_TOOLCHAIN_FILE="$toolchain" -DCMAKE_INSTALL_PREFIX=/usr/arm-frc-linux-gnueabi -DCMAKE_BUILD_TYPE=Release .
+make install $make_args -j $num_jobs
+
+# bondcpp v1.8.3 (Aug 17 2018) requires uuid. Corresponding version v2.32.1
+# Note: There might be a way to do with NI's ipks using ossp-libuuid. Not sure, haven't tried
+wget -q --show-progress https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v2.32/util-linux-2.32.1.tar.gz
+tar xzf util-linux-2.32.1.tar.gz
+cd util-linux-2.32.1
+./configure --host=arm-frc-linux-gnueabi --prefix=/usr/arm-frc-linux-gnueabi/usr/local --disable-all-programs --enable-libuuid
+make install $make_args -j $num_jobs
+
+# Cleanup
+cd
+rm -rf .frc_control_temp
